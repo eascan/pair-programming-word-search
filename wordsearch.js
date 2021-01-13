e11eab4fb09010c03edec4baa80c7cb2fea73b9b
Edward <edward.ascan@me.com>
2021-01-13 22:50:38 +0000

    worked on tests and creating the wordsearch function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,52 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Worked in pair programming with @Apurva Jani
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+    let newArray = [];
+    let numPlaceholders = 0;
+    
+    for (let i = 0; i < matrix.length; i++) {
+      numPlaceholders = matrix[i].length;
+    }
+    
+    for (let j = 0; j < numPlaceholders; j++) { // building a new empty placeholder set
+      newArray.push([]);
+    }
+    // newArray = [ [], [], [], [], [], [], [], ]   .length = 7
+    // newArray[j]  ^
+  
+    for (let j = 0; j < newArray.length; j++) { // looping through newArray
+      for (let x = 0; x < matrix.length; x++) {
+        newArray[j].push(matrix[x][j]); // to each j index of newArray, add matrix[x][j]
+      }
+    }
+    return newArray;
+  };
+  
+  
+  
+  const wordSearch = (letters, word) => {
+    const verticalFlipped = transpose(letters);
+    const verticalJoin = verticalFlipped.map(ls => ls.join(''));
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    
+    if (letters === [] || word === '') {
+      return undefined;
+    }
+
+    for (strings of horizontalJoin) {
+        if (strings.includes(word)) {
+            return true;
+        }  
+    }
+
+    for (strings of verticalJoin) {
+        if (strings.includes(word)) {
+            return true;
+        }
+    }
+
+    return false;
+
+  };
+
+module.exports = wordSearch;
